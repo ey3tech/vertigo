4685c2cbf8453ba868c1bf9f34be61f3d9ed41a4
terabyte3 <terabyte@terabyteis.me>
2021-09-20 14:19:40 +0000

    make the form

--- cmd/brute.go
+++ cmd/brute.go
@@ -17,29 +17,65 @@ package cmd
 
 import (
 	"errors"
+	"github.com/go-ping/ping"
 	"fmt"
-	_ "net"
-	_ "time"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"github.com/AlecAivazis/survey/v2"
 )
 
 // bruteCmd represents the brute command
-var service string
-var port int
+var qs = []*survey.Question{
+	{
+        Name:     "hostname",
+        Prompt:   &survey.Input{Message: "who are we cracking today?"},
+        Validate: func (ans interface{}) error {
+			// check if the hostname is invalid
+			if str, ok := ans.(string) ; !ok {
+				return errors.New(color.RedString("invalid hostname"))
+			} else if _, ok := ping.NewPinger(str); ok != nil {
+				return errors.New(color.RedString("invalid hostname"))
+			}
+			return nil
+		},
+		Transform: survey.ToLower,
+    },
+    {
+        Name: "service",
+        Prompt: &survey.Select{
+            Message: "what service are we attacking?",
+            Options: []string{"http(s)", "ssh", "ftp"},
+            Default: "ssh",
+        },
+    },
+    {
+        Name: "confirm",
+        Prompt: &survey.Select{
+			Message: "ready?",
+			Options: []string{color.RedString("[x] no, i'm too baby"), color.GreenString("[+] let's goooo")},
+			Default: 1,
+		},
+    },
+}
 var bruteCmd = &cobra.Command{
 	Use:   "brute",
 	Short: "crack a password",
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New(color.RedString("missing host to bruteforce"))
+	RunE: func(cmd *cobra.Command, args []string) error {
+		answers := struct {
+			Hostname string
+			Service string
+			Confirm int
+		}{}
+		if answers.Confirm == 0 { // index of the options list above
+			fmt.Println(color.CyanString("[i] ") + "canceled")
+			return nil
 		}
-		//conn, err := net.DialTimeout("tcp", args[0], time.Duration(timeout)*time.Millisecond)
+		err := survey.Ask(qs, &answers)
+		if err != nil {
+			return errors.New(color.RedString(err.Error()))
+		}
 		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("brute called")
 	},
 }
 
